540042f247f0de1b50e12f87e22f1ba696a73de0
praneeth <muppasanipraneeth19@gmail.com>
2024-07-25 12:13:05 +0530

    learned about the routing and it's hooks

--- learning/src/app.js
+++ learning/src/app.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import {createBrowserRouter,RouterProvider,Outlet} from "react-router-dom"
 
 /* 
 Header 
@@ -35,14 +39,34 @@ footer
 const Appcontent=()=>{
     return (<>
         <Header />
-        <Body></Body>
+        <Outlet />
         
         </>)
 }
 
-
+const appRouter=createBrowserRouter([
+  {
+    path :"/",
+    element :<Appcontent />,
+    children :[
+      {
+        path:"/",
+        element :<Body />
+      },
+      {
+      path:"/about",
+      element:<About />
+    },
+    {
+      path:"/contactus",
+      element :<Contact/>
+    }],
+    errorElement :<Error />
+  },
+  
+]);
 
 const root =ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Appcontent/>);
+root.render(<RouterProvider  router={appRouter}></RouterProvider>);
 
 
---
+++ learning/src/components/About.js
@@ -0,0 +1,13 @@
+const About=()=>{
+    return(<>
+    <div>
+        <h1> About us</h1>
+        <h4> contact details: 9022*****</h4>
+        <h4> Emali: muppasani@*****</h4>
+        <p> Adress : kurnool district 518004
+            abdul khan estate
+        </p>
+    </div>
+    </>);
+}
+export default About;
